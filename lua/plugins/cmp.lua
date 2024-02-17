e53d3a07a80fc860a3f034a79be642ee8ef45fa3
AtleSkaanes <atle.skaanes@gmail.com>
2024-02-17 15:49:08 +0100

    + autopairs, - autoclose

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,56 @@
+return {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-nvim-lsp',
+        -- LuaSnip
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local lsp_zero = require('lsp-zero')
+
+        cmp.setup({
+            sources = {
+                { name = 'path' },
+                { name = 'nvim_lsp' },
+                { name = 'nvim_lua' },
+            },
+            formatting = lsp_zero.cmp_format(),
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+        })
+
+        -- `/` cmdline setup.
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' },
+            },
+        })
+
+        -- `:` cmdline setup.
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' },
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' },
+                    },
+                },
+            }),
+        })
+    end,
+}
